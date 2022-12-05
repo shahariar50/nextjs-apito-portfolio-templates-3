84544876b79a947ac308fcb32af25e4b33468d9d
shahariar50 <shahariar.touhid@gmail.com>
2022-12-05 06:42:15 +0600

    setup the filter

--- components/FilterSection.jsx
+++ components/FilterSection.jsx
@@ -1,202 +1,148 @@
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const FilterSection = () => {
+  const [selectedFilter, setSelectedFilter] = useState("all");
+
+  const filterList = [
+    { slag: "all", title: "All" },
+    { slag: "digital", title: "Digital" },
+    { slag: "web_design", title: "Web Design" },
+    { slag: "marketing", title: "Marketing" },
+  ];
+
+  const filterImages = [
+    {
+      slag: "img1",
+      img: "/static/photo-1558212628-ad7f5fb28c5e.jpg",
+      filterTags: ["all", "digital"],
+      title: "Aramark",
+      category: "Marketing",
+    },
+    {
+      slag: "img2",
+      img: "/static/photo-1558180702-95f1c3ae2ca3.jpg",
+      filterTags: ["all", "marketing"],
+      title: "Portfolio page",
+      category: "Web design",
+    },
+    {
+      slag: "img3",
+      img: "/static/photo-1558082513-13668c3b7425.jpg",
+      filterTags: ["all", "marketing"],
+      title: "Sunday Breakfast",
+      category: "Branding",
+    },
+    {
+      slag: "img4",
+      img: "/static/photo-1558100984-01e6cd6fc9aa.jpg",
+      filterTags: ["all", "marketing"],
+      title: "Sunday Breakfast",
+      category: "All",
+    },
+    {
+      slag: "img5",
+      img: "/static/photo-1557941760-987c3f403d5a.jpg",
+      filterTags: ["all", "marketing", "digital"],
+      title: "Pine Street",
+      category: "Web + Digital",
+    },
+    {
+      slag: "img6",
+      img: "/static/photo-1534073828943-f801091bb18c.jpg",
+      filterTags: ["all", "web_design", "digital"],
+      title: "Mobipaid",
+      category: "Branding",
+    },
+    {
+      slag: "img7",
+      img: "/static/photo-1484755560615-a4c64e778a6c.jpg",
+      filterTags: ["all", "web_design"],
+      title: "Solo Solution",
+      category: "Logo",
+    },
+    {
+      slag: "img8",
+      img: "/static/photo-1494253109108-2e30c049369b.jpg",
+      filterTags: ["all", "web_design", "marketing"],
+      title: "Panic web",
+      category: "Branding Campaign",
+    },
+    {
+      slag: "img9",
+      img: "/static/photo-1436262513933-a0b06755c784.jpg",
+      filterTags: ["all", "web_design", "digital"],
+      title: "Solocademy",
+      category: "Web design + Branding",
+    },
+    {
+      slag: "img10",
+      img: "/static/photo-1494475673543-6a6a27143fc8.jpg",
+      filterTags: ["all", "web_design", "digital"],
+      title: "Soulart",
+      category: "Marketing",
+    },
+    {
+      slag: "img11",
+      img: "/static/photo-1501534664411-d04203736d05.jpg",
+      filterTags: ["all", "marketing", "web_design", "digital"],
+      title: "DuleSavic",
+      category: "Fudbaler",
+    },
+  ];
+
+  const handleFilterClick = (e, text) => {
+    e.preventDefault();
+    setSelectedFilter(text);
+  };
+
   return (
     <section className="space-md">
       <div id="portfolio" className="container">
         <div id="portfolio-filters">
           <ul id="filters" className="p-0">
-            <li>
-              <a href="*" className="active">
-                All
-              </a>
-            </li>
-            <li>
-              <a href=".digital">Digital</a>
-            </li>
-            <li>
-              <a href=".branding">Web Design</a>
-            </li>
-            <li>
-              <a href=".campaigns">Marketing</a>
-            </li>
+            {filterList.map((filter) => (
+              <li key={filter?.slag}>
+                <Link
+                  href="#"
+                  onClick={(e) => handleFilterClick(e, filter?.slag)}
+                  className={filter?.slag === selectedFilter ? "active" : ""}
+                >
+                  {filter?.title}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
-        <div
-          className="grid"
-          data-cols="3"
-          data-margin="30"
-          data-height="1"
-          data-masonry='{ "columnWidth": 200, "itemSelector": ".entry" }'
-        >
-          <div className="entry work-entry branding">
-            <a href="project.html">
-              <div
-                className="entry-image imageBG"
-                data-img="img/photo-1558212628-ad7f5fb28c5e.jpg"
-              ></div>
-              <div className="work-entry-hover">
-                <div className="work-entry-content">
-                  <div className="work-entry-title">Liber Finance</div>
-                  <div className="work-entry-cat">Web</div>
+        <div className="filterWrapper">
+          {filterImages.map(
+            (filterImage) =>
+              filterImage?.filterTags?.includes(selectedFilter) && (
+                <div
+                  key={filterImage?.slag}
+                  className="entry work-entry branding"
+                >
+                  <div
+                    className="entry-image imageBG"
+                    style={{ backgroundImage: `url(${filterImage?.img})` }}
+                  >
+                    <picture>
+                      <img src={filterImage?.img} alt={filterImage?.slag} />
+                    </picture>
+                  </div>
+                  <div className="work-entry-hover">
+                    <div className="work-entry-content">
+                      <div className="work-entry-title">
+                        {filterImage?.title}
+                      </div>
+                      <div className="work-entry-cat">
+                        {filterImage?.category}
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </a>
-          </div>
-          <div className="entry work-entry digital">
-            <a href="project.html">
-              <div
-                className="entry-image imageBG"
-                data-img="img/photo-1558180702-95f1c3ae2ca3.jpg"
-              ></div>
-              <div className="work-entry-hover">
-                <div className="work-entry-content">
-                  <div className="work-entry-title">Aramark</div>
-                  <div className="work-entry-cat">Marketing</div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div className="entry work-entry campaigns">
-            <a href="project.html">
-              <div
-                className="entry-image imageBG"
-                data-img="img/photo-1558118720-fa5cdebe6b3a.jpg"
-              ></div>
-              <div className="work-entry-hover">
-                <div className="work-entry-content">
-                  <div className="work-entry-title">Portfolio page</div>
-                  <div className="work-entry-cat">web design</div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div className="entry work-entry branding">
-            <a href="project.html">
-              <div
-                className="entry-image imageBG"
-                data-img="img/photo-1558100984-01e6cd6fc9aa.jpg"
-              ></div>
-              <div className="work-entry-hover">
-                <div className="work-entry-content">
-                  <div className="work-entry-title">Sunday Breakfast</div>
-                  <div className="work-entry-cat">Branding</div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div className="entry work-entry campaigns h2">
-            <a href="project.html">
-              <div
-                className="entry-image imageBG"
-                data-img="img/photo-1558082513-13668c3b7425.jpg"
-              ></div>
-              <div className="work-entry-hover">
-                <div className="work-entry-content">
-                  <div className="work-entry-title">Sunday Breakfast</div>
-                  <div className="work-entry-cat">All</div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div className="entry work-entry branding digital">
-            <a href="project.html">
-              <div
-                className="entry-image imageBG"
-                data-img="img/photo-1557941760-987c3f403d5a.jpg"
-              ></div>
-              <div className="work-entry-hover">
-                <div className="work-entry-content">
-                  <div className="work-entry-title">Pine Street</div>
-                  <div className="work-entry-cat">Web + Digital</div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div className="entry work-entry digital">
-            <a href="project.html">
-              <div
-                className="entry-image imageBG"
-                data-img="img/photo-1534073828943-f801091bb18c.jpg"
-              ></div>
-              <div className="work-entry-hover">
-                <div className="work-entry-content">
-                  <div className="work-entry-title">Mobipaid</div>
-                  <div className="work-entry-cat">Branding</div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div className="entry work-entry brading">
-            <a href="project.html">
-              <div
-                className="entry-image imageBG"
-                data-img="img/photo-1484755560615-a4c64e778a6c.jpg"
-              ></div>
-              <div className="work-entry-hover">
-                <div className="work-entry-content">
-                  <div className="work-entry-title">Solo Solution</div>
-                  <div className="work-entry-cat">Logo</div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div className="entry work-entry digital campaigns">
-            <a href="project.html">
-              <div
-                className="entry-image imageBG"
-                data-img="img/photo-1494253109108-2e30c049369b.jpg"
-              ></div>
-              <div className="work-entry-hover">
-                <div className="work-entry-content">
-                  <div className="work-entry-title">Panic web</div>
-                  <div className="work-entry-cat">Branding Campaign</div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div className="entry work-entry branding digital">
-            <a href="project.html">
-              <div
-                className="entry-image imageBG"
-                data-img="img/photo-1436262513933-a0b06755c784.jpg"
-              ></div>
-              <div className="work-entry-hover">
-                <div className="work-entry-content">
-                  <div className="work-entry-title">Solocademy</div>
-                  <div className="work-entry-cat">Web design + Branding</div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div className="entry work-entry campaigns digital h2">
-            <a href="project.html">
-              <div
-                className="entry-image imageBG"
-                data-img="img/photo-1494475673543-6a6a27143fc8.jpg"
-              ></div>
-              <div className="work-entry-hover">
-                <div className="work-entry-content">
-                  <div className="work-entry-title">Soulart</div>
-                  <div className="work-entry-cat">Marketing</div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div className="entry work-entry campaigns digital branding w2">
-            <a href="project.html">
-              <div
-                className="entry-image imageBG"
-                data-img="img/photo-1501534664411-d04203736d05.jpg"
-              ></div>
-              <div className="work-entry-hover">
-                <div className="work-entry-content">
-                  <div className="work-entry-title">DuleSavic</div>
-                  <div className="work-entry-cat">Fudbaler</div>
-                </div>
-              </div>
-            </a>
-          </div>
+              )
+          )}
         </div>
       </div>
     </section>
